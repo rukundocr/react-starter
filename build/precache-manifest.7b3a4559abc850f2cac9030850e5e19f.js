3a086e3b98fc81c420b6fe8a79a4d0685081e25e
rukundo jean claude <rukundocr@gmail.com>
2020-10-12 20:45:46 +0200

    add build

---
+++ build/precache-manifest.7b3a4559abc850f2cac9030850e5e19f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17f3e99baa4668d23b7d3aee743e2706",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0d335053050f1b6959c",
+    "url": "/static/css/main.5f02eabc.chunk.css"
+  },
+  {
+    "revision": "91b1220517d4b16f9df2",
+    "url": "/static/js/2.7d9390b4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7d9390b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0d335053050f1b6959c",
+    "url": "/static/js/main.19232bad.chunk.js"
+  },
+  {
+    "revision": "8fc0864ec30d916942ba",
+    "url": "/static/js/runtime-main.4fe0d2d7.js"
+  }
+]);
